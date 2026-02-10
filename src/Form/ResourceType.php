3c4081e0eefaae47dfafdafb884e9f4ef1b66c1a
Ahmed Omry <ahmedomri.dev@gmail.com>
2026-02-10 21:21:49 +0100

    Resource&comment

---
+++ src/Form/ResourceType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Resource;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResourceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['rows' => 4],
+            ])
+            ->add('filePath', TextType::class, [
+                'label' => 'Chemin du fichier',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Resource::class,
+        ]);
+    }
+}
